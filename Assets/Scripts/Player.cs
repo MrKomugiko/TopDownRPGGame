8b832d948034191a7117b0ff5ba11cc1c5b0eedf
MrKomugiko <Mr.Komugiko@gmail.com>
2019-11-03 23:27:05 +0100

    dodanie UI health i mana pasków, dodawanie i odejmowanie zycia

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Player : Character
 {
+    [SerializeField]
+    private Stat health;
+    private float initHealth = 100;
+    [SerializeField]
+    private Stat mana;
+    private float initMana = 200;
     protected override void Start() {
+        health.Initialize(initHealth, initHealth);
+        mana.Initialize(initMana, initMana);
         base.Start();
     }
-
    protected override void Update() {
         GetInput();
         base.Update();
@@ -15,6 +22,17 @@ public class Player : Character
 
     private void GetInput() {
         direction = Vector2.zero;
+
+        //SPRAWDZENIE CZY PASKI HP DZIALAJA POPRAWNIE
+        if (Input.GetKeyDown(KeyCode.I)) {
+            health.MyCurrentValue -= 10;
+            mana.MyCurrentValue -= 50;
+        }
+        if (Input.GetKeyDown(KeyCode.O)) {
+            health.MyCurrentValue += 10;
+            mana.MyCurrentValue += 50;
+        }
+
         if (Input.GetAxisRaw("Vertical") > 0) {
             direction += Vector2.up;
         }
---
+++ Assets/Scripts/Stat.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stat : MonoBehaviour {
+    private Image content;
+    private float currentFill;
+    private float currentValue;
+    [SerializeField]
+    private float lerpSpeed;
+    public float MyMaxValue { get; set; }
+    public float MyCurrentValue {
+        get {
+            return currentValue;
+        }
+        set {
+            if (value > MyMaxValue) {
+                currentValue = MyMaxValue;
+            } else if (value < 0) {
+                currentValue = 0;
+            } else currentValue = value;
+            currentFill = currentValue / MyMaxValue;
+        }
+    }
+
+    void Start() {
+        content = GetComponent<Image>();
+    }
+
+    void Update() {
+        if(currentFill != content.fillAmount) {
+            content.fillAmount = Mathf.Lerp(content.fillAmount, currentFill, Time.deltaTime * lerpSpeed);
+        }
+        //content.fillAmount = currentFill;
+    }
+
+    public void Initialize(float currentValue, float maxValue) {
+        MyMaxValue = maxValue;
+        MyCurrentValue = currentValue;
+    }
+}
+
